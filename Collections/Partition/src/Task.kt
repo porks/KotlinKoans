fc9c906492ea414fea82c1bd4eb194c5f439dd0c
Marcelo Rossi <marcelo.rossi@gmail.com>
2021-08-20 18:21:34 -0300

    Module Collections complete

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,10 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> {
+//    return customers.filter { it.orders.count { order -> !order.isDelivered } > it.orders.count(Order::isDelivered) }.toSet()
+    return customers.filter {
+        val (delivery, undelivery) = it.orders.partition(Order::isDelivered)
+        undelivery.size > delivery.size
+    }.toSet()
+
+
+}
